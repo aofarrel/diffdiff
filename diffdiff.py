f1caf581ec2bb7ecc4730aa1ed707d3e3f42d917
Ash O'Farrell <aofarrel@ucsc.edu>
2023-11-01 15:54:29 -0700

    Add backmasking, pylint

--- diffdiff.py
+++ diffdiff.py
@@ -1,15 +1,17 @@
 # this comparison will drop dashes!
-import itertools
+# pylint: disable=W0311,W1514,C0103,C0321,C0301
+
 import sys
 
 with open(sys.argv[1]) as pile_of_diffs:
-    diffs = [line.strip("\n") for line in pile_of_diffs.readlines()]
-    
-black = '\033[30m'
-red = '\033[91m'
-end = '\033[0m'
-highlight_cyan = '\u001b[48;5;87m'
-highlight_green = '\u001b[48;5;47m'
+	diffs = [line.strip("\n") for line in pile_of_diffs.readlines()]
+backmask = sys.argv[2]
+
+BLACK = '\033[30m'
+RED = '\033[91m'
+END = '\033[0m'
+HIGHLIGHT_CYAN = '\u001b[48;5;87m'
+HIGHLIGHT_GREEN = '\u001b[48;5;47m'
 
 diffionaries = {}
 for diff in diffs:
@@ -30,27 +32,41 @@ for i in range(0, len(diffs)):
 		if position not in all_positions:
 			all_positions.append(position)
 
-all_bad_ones = []
+incongruent_positions = []
 for position in all_positions:
 	highlight_this_position = False
 	each_sample = []
-	for sample in diffionaries:
-		if position not in diffionaries[sample].keys():
+	for sample, positions in diffionaries.items():
+		if position not in positions.keys():
 			highlight_this_position = True
-			if position not in all_bad_ones: all_bad_ones.append(position)
-			each_sample.append(f"{red}-{black}")
+			if position not in incongruent_positions: incongruent_positions.append(position)
+			each_sample.append(f"{RED}-{BLACK}")
 		else:
-			each_sample.append(diffionaries[sample][position])
+			each_sample.append(positions[position])
 	samples_at_this_position = ''.join(sample for sample in each_sample)
 
+	# print in place -- likely more efficient then going back later
 	if highlight_this_position:
-		print(f"{highlight_cyan}{position}\t{''.join(sample for sample in each_sample)}{end}")
-	elif not samples_at_this_position.count(samples_at_this_position[0]) == len(samples_at_this_position):
-		print(f"{highlight_green}{position}\t{''.join(sample for sample in each_sample)}{end}")
+		print(f"{HIGHLIGHT_CYAN}{position}\t{''.join(sample for sample in each_sample)}{END}")
+	elif samples_at_this_position.count(samples_at_this_position[0]) != len(samples_at_this_position):
+		print(f"{HIGHLIGHT_GREEN}{position}\t{''.join(sample for sample in each_sample)}{END}")
 	else:
 		print(f"{position}\t{''.join(sample for sample in each_sample)}")
 
 for sample, diff in diffionaries.items():
-    print(f"{sample} calls {len(diff)} SNPs")
+	print(f"{sample} calls {len(diff)} SNPs")
 
-print(f"{len(all_bad_ones)} out of {len(all_positions)} positions have at least one mismatch")
+print(f"{len(incongruent_positions)} out of {len(all_positions)} positions have at least one mismatch")
+
+if backmask:
+	for input_diff_file in diffs:
+		this_sample_dic = diffionaries[input_diff_file]
+		this_sample_backmasked = []
+		with open(f"{input_diff_file}.backmask.diff", "w") as backmasked_diff:
+			for position in this_sample_dic:
+				if position in incongruent_positions:
+				  backmasked_diff.write(f"-\t{position}\t1\n")
+				  this_sample_backmasked.append(position)
+				else:
+				  backmasked_diff.write(f"{this_sample_dic[position]}\t{position}\t1\n")
+		print(f"For {input_diff_file}, backmasked {len(this_sample_backmasked)} positions at: {this_sample_backmasked}")
